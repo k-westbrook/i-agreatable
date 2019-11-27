4c704596ee9e359588431564bd6ab907d5c0f27d
k-westbrook <kcheriewestbrook@gmail.com>
2019-11-26 18:16:05 -0800

    created filter container

---
+++ src/FilterContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import { Link } from 'react-router-dom';
+
+export default function FilterContainer(props) {
+  return (
+    <div>
+
+    </div>
+  )
+}
