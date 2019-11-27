00785c11b765c961e348c357b7819715229d98af
k-westbrook <kcheriewestbrook@gmail.com>
2019-11-26 20:32:21 -0800

    cleaned up code

--- src/RestaurantList.js
+++ src/RestaurantList.js
@@ -4,7 +4,6 @@ import RestaurantCardContainer from './RestaurantCardContainer';
 import axios from 'axios';
 import FilterContainer from './FilterContainer';
 
-
 class RestaurantList extends React.Component {
 
   constructor(props) {
@@ -14,46 +13,55 @@ class RestaurantList extends React.Component {
       filterFoodType: 'Select One',
       filterLocation: 'Select One'
     }
+
     this.handleFoodFilterChange = this.handleFoodFilterChange.bind(this);
     this.handleLocationFilterChange = this.handleLocationFilterChange.bind(this);
     this.getRestaurants = this.getRestaurants.bind(this);
+
   }
   async getRestaurants() {
+
     let result = await axios.get(
       "https://0uz9m4vuz3.execute-api.us-west-1.amazonaws.com/Production"
     )
 
     this.setState({ restaurants: result.data.body });
+
   }
 
-
   handleFoodFilterChange(evt) {
+
     evt.preventDefault();
     this.setState({
       filterFoodType: evt.target.value
     })
+
   }
 
   handleLocationFilterChange(evt) {
+
     evt.preventDefault();
     this.setState({
       filterLocation: evt.target.value
     })
+
   }
+
   componentDidMount() {
+
     this.getRestaurants();
+
   }
+
   render() {
 
     return (
       <div >
-
         <p className="header-restaurant">Restaurant List</p>
         <FilterContainer handleFoodFilterChange={this.handleFoodFilterChange} handleLocationFilterChange={this.handleLocationFilterChange} />
         <div>
           <RestaurantCardContainer restaurants={this.state.restaurants} foodType={this.state.filterFoodType} location={this.state.filterLocation} />
         </div>
-
       </div>
     );
   }
